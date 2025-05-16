e9963410d2e9c57ea034b3a064834f62897516e5
cristian.vanegas <cristian.vanegas@aplicando.co>
2025-05-16 17:43:07 -0500

    WebApplication

---
+++ WebApplication1/WebApplication1/Contexts/MongoDbContext.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Driver;
+
+namespace WebApplication1.Contexts
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoClient _mongoClient;
+        private readonly IMongoDatabase _databaseService;
+
+        public MongoDbContext(IConfiguration configuration)
+        {
+            var connectionString = configuration["ConnectionStrings"];
+            var dbName = "Product";
+
+
+            // Create Client MongoDb
+            _mongoClient = new MongoClient(connectionString);
+
+            // Get Database
+            _databaseService = _mongoClient.GetDatabase(dbName);
+        }
+
+        public IMongoCollection<T> GetCollectionService<T>(string name)
+        {
+            return _databaseService.GetCollection<T>(name);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Services/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using WebApplication1.Contexts;
+using WebApplication1.Model;
+
+namespace WebApplication1.Services
+{
+    public class ProductService
+    {
+        readonly IMongoCollection<Product> _collection;
+
+        public ProductService(MongoDbContext mongoDbContext)
+        {
+            _collection = mongoDbContext.GetCollectionService<Product>("Product");
+        }
+
+        public async Task<bool> CreateProduc(Product product)
+        {
+            await _collection.InsertOneAsync(product);
+
+            return true;
+        }
+    }
+}
